21690fe2736ddd9edd4b7c63d3ca765814454f36
Muhammadfayyozbek Ahmadjonov <usmonovbahriddin108@gmail.com>
2024-04-02 21:24:15 +0500

    [ADD] final homework

--- index6.js
+++ index6.js
@@ -1,11 +1,11 @@
 let year = +prompt("Yilni kiriting:");
 let month = +prompt("Oyni raqamda kiriting:");
 let day = +prompt("Kunni kiriting:");
-function calculateDays(year, month, day) {
+function totalDays(year, month, day) {
   let startDate = new Date(1970, 0, 1);
   let endDate = new Date(year, month - 1, day);
-  let timeDifference = endDate.getTime() - startDate.getTime();
-  let daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-  alert(daysDifference);
+  let milisekundFarqi = endDate.getTime() - startDate.getTime();
+  let kunfarqi = Math.floor(milisekundFarqi / (1000 * 60 * 60 * 24));
+  alert(kunfarqi);
 }
-calculateDays(year, month, day);
+totalDays(year, month, day);
